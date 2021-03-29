4a01d839c2f5a7221a5f99af77af7abeefa06ddf
NabiKalokoh <nabiatu1@gmail.edu>
2021-03-29 15:46:34 -0400

    Made tutorial text file and added Help button.

--- loginPage/static/js/tutorial.js
+++ loginPage/static/js/tutorial.js
@@ -1,3 +1,7 @@
-document.getElementById('here').addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-});
+
+function need_help() {
+    document.getElementById('here').addEventListener('click', function () {
+        document.querySelector('.bg-modal').style.display = 'flex';
+    });
+
+}
