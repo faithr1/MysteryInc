adb21e7db0efae4a0dd7e2630e021141f92089f2
NabiKalokoh <nabiatu1@gmail.edu>
2021-03-31 18:23:18 -0400

    Small additions to tutorialText.txt.

--- loginPage/static/js/tutorial.js
+++ loginPage/static/js/tutorial.js
@@ -1,6 +1,6 @@
 
 function need_help() {
-    document.getElementById('here').addEventListener('click', function () {
+    document.getElementById('help').addEventListener('click', function () {
         document.querySelector('.bg-modal').style.display = 'flex';
     });
 
