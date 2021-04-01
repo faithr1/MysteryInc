4290bf3cd1699778eb8dcfcea01c091005aad70d
NabiKalokoh <nabiatu1@gmail.edu>
2021-04-01 15:01:22 -0400

    Added a few comments.

--- loginPage/static/js/tutorial.js
+++ loginPage/static/js/tutorial.js
@@ -1,4 +1,5 @@
-
+/* clicking on the help button should make the hidden popup box appear
+*  currently isn't working. */
 function need_help() {
     document.getElementById('help').addEventListener('click', function () {
         document.querySelector('.bg-modal').style.display = 'flex';
