13974ee7750f85cf8ced0bc13d1ddc7bbe38aeca
NabiKalokoh <nabiatu1@gmail.edu>
2021-04-29 15:59:49 -0400

    Attempted to add more tests.

--- tests.py
+++ tests.py
@@ -10,10 +10,12 @@ time.sleep(2)  # Wait 2 secs
 self.selenium.find_element_by_id('help').click() #create pop-up box
 self.selenium.find_element_by_id('help').click()
 time.sleep(2) #wait 2 secs
+self.assertEqual('.bg-modal'.style.display, 'flex')
 
 #close the pop-up box
 self.selenium.find_element_by_class_name('close').click()
 time.sleep(2) #wait 2 seconds
+self.assertEqual('.bg-modal'.style.display, 'none')
 
 #checking if the function returned true after opening and closing pop up box
 self.assertTrue(need_help(), 0)
